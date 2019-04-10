3a8624a891d6a55ee01bc62cc2c28b90abdd42a6
Andrei Kramer <kramer.ndr@gmail.com>
2019-04-10 15:40:20 +0200

    simplified ods to tsv conversion, names are now general, derived from ods file

--- ods_to_tsv.sh
+++ ods_to_tsv.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
+
 if (($#>0)); then
-    echo  "ssconvert -S -T Gnumeric_stf:stf_csv ${1}.ods CaMKII_%s.tsv"
-    ssconvert -S -T "Gnumeric_stf:stf_csv" ${1} 'CaMKII_%s.tsv'
-    #ssconvert -S -T Gnumeric_stf:stf_assistant --export-options="quoting-mode=never separator=	" ${1} 'CaMKII_%s.tsv'
-    
-    for i in CaMKII_*.tsv; do
-     	sed -i -E 's/,/\t/g' "${i}"
-    done
+    Name=${1%%.ods}
+    ssconvert -S --export-options="quoting-mode=never separator='	'" ${1} "${Name}_%s.txt"
+    for i in ${Name}_*.txt; do
+	mv "${i}" "${i%%txt}tsv";
+    done    
 fi
