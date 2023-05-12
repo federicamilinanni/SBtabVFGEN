174659c9ea98a8a9f5fc8edbe6b258f02924b156
Andrei Kramer-Miehe <andrei.kramer@scilifelab.se>
2023-05-12 18:36:04 +0200

    corrected test, removed default test

--- tests/testthat/test-units.R
+++ tests/testthat/test-units.R
@@ -1,7 +1,3 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
-
 test_that("unicode micro works", {
 	u<-unit.from.string("µM")
 	expect_equal(u$exponent,c(1,-1))
@@ -9,5 +5,8 @@ test_that("unicode micro works", {
 
 test_that("fractions work", {
 	u<-unit.from.string("km/h")
-	expect_equal(u,data.frame(scale=c(3,0),multiplier=c(1,60),exponent=c(1,-1)))
+	U <- data.frame(scale=c(3,0),multiplier=c(1,60),exponent=c(1,-1),kind=c('metre','second'))
+	comment(U) <- "km/h"
+	attr(U,'id') <- "km_per_h"
+	expect_equal(u,U)
 })
